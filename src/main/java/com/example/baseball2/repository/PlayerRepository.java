f4aa06b1966f37d2e302994c3dfa383e53b220c8
ozaki25 <fs65ugumori@yahoo.co.jp>
2018-11-29 18:03:02 +0900

    init

---
+++ src/main/java/com/example/baseball2/repository/PlayerRepository.java
@@ -0,0 +1,11 @@
+package com.example.baseball2.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.baseball2.domain.Player;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+
+}
